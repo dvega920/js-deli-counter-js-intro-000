e9af277c52ca813290d2adeb56268dffd5e84df2
david.vega@snhu.edu <david.vega@snhu.edu>
2018-09-08 15:08:20 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+
+function takeANumber(katzDeliLine, name){
+	katzDeliLine.push(name)
+	return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing(katzDeliLine){
+  var firstPerson = [];
+	if (katzDeliLine.length < 1){
+		return `There is nobody waiting to be served!`
+	} else {
+	  firstPerson.push(katzDeliLine[0]);
+	  katzDeliLine.shift();
+	  return `Currently serving ${firstPerson}.`
+	}
+}
